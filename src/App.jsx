ef8389cf98dc7c6ac5cbb9b5c27ea2151826e25b
ThofikhBisyron <sanada.ryuzaki1@gmail.com>
2024-08-28 17:50:57 +0700

    feat: add Component OrderStatic in DashboardAdmin Page

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Login from "./pages/Login";
 import UserList from "./pages/UserList";
 import HomePage from "./pages/HomePage";
 import DashboardAdmin from "./pages/DashboardAdmin";
-import UserList from "./pages/UserList";
+// import UserList from "./pages/UserList";
 import Profile from "./pages/Profile";
 
 const router = createBrowserRouter([
--- src/pages/DashboardAdmin.jsx
+++ src/pages/DashboardAdmin.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import CardInformation from "../components/CardInformation";
+import OrderStatic from "../components/OrderStatic";
 
-function DashboarAdmin(){
+function DashboardAdmin(){
     return(
         <div>
             <CardInformation/>
+            <OrderStatic/>
         </div>
     )
 }
-export default DashboarAdmin
+export default DashboardAdmin
