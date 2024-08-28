5d818c3d940732e6f2d60d4846587c24bc3206c4
fajryalvin12 <fajryalvin12@gmail.com>
2024-08-28 14:22:32 +0700

    feat:add home testimonial slicing

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import HomeGridFavourite from "../components/HomeGridFavourite";
 import HomeComponent from "../components/HomeDescription";
+import HomeTestimonial from "../components/HomeTestimonial";
+import Navbar from "../component/Navbar";
+import Footer from "../component/Footer";
 
 function HomePage() {
-  return (<HomeComponent />
-         <HomeGridFavourite />);
+  return (
+    <>
+      <Navbar />
+      <HomeComponent />
+      <HomeGridFavourite />
+      <HomeTestimonial />
+      <Footer />
+    </>
+  );
 }
 
 export default HomePage;
