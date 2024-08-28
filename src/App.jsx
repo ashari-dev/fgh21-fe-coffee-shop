31dbb03a45a139334e8295b1a83c52e354f2e11f
syarif <syarif.khalid@gmail.com>
2024-08-28 13:41:38 +0700

    app.jsx component chat admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import EditProduct from "./pages/EditProduct";
+import ChatAdmin from "./pages/ChatAdmin";
 
 const router = createBrowserRouter([
   {
     path: "/EditProduct",
     element: <EditProduct />,
+    path: "/chat",
+    element: <ChatAdmin />,
   },
 ]);
 
