f8f4a9371f1457bd03077c9296db03a0ab2a6c5b
fajryalvin12 <fajryalvin12@gmail.com>
2024-08-28 11:24:18 +0700

    feat: add gridproduct components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import React from "react";
+import GridProduct from "./components/GridProduct";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <GridProduct />
+    </>
+  );
 }
 
 export default App;
