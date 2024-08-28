582fdcc90ffc37be612101af3fb86fb7713769aa
ilyas <ilyasalqordhowi@gmail.com>
2024-08-28 11:26:24 +0700

    feat:Login and component Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login";
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <Login />,
+  },
+]);
+
 function App() {
-  return <></>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
