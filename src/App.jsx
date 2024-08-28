deaaf7aa74680b7a9e880201392c35107d92476e
daffa <l200170028@student.ums.ac.id>
2024-08-28 18:20:10 +0700

    feat: new page detail order

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,36 @@
 import React from "react";
 import Forgetpassword from "./pages/Forgetpassword";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import EditProduct from "./pages/EditProduct";
+// import EditProduct from "./pages/EditProduct";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import DetailOrder from "./pages/DetailOrder";
+// import DetailProduct from "./pages/DetailProduct";
 
 const router = createBrowserRouter([
   {
     path: "/Forgetpassword",
     element: <Forgetpassword />,
   },
-  {
-    path: "/EditProduct",
-    element: <EditProduct />,
-  },
+  // {
+  //   path: "/EditProduct",
+  //   element: <EditProduct />,
+  // },
   {
   path: "/register",
   element: <Register />,
   },
+  {
+  path: "/login",
+  element: <Login />,
+  },
+  // {
+  //   path: "/detail-product",
+  //   element: <DetailProduct />,
+  //   },
     {
-    path: "/login",
-    element: <Login />,
+      path: "/detail-order",
+      element: <DetailOrder />,
     },
 ]);
 
