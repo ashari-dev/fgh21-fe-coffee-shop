bcc4ea60a192872c22d87379b70dbbf2588dffe5
daffa <l200170028@student.ums.ac.id>
2024-09-01 17:11:42 +0700

    feat:integration promo

--- src/components/PromoList.jsx
+++ src/components/PromoList.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
 import Coupon from "../assets/img/coupon.png"
+import { useEffect } from "react";
 
 function PromoList() {
+    const [promos, setPromos] = React.useState([]);
+    async function promo() {
+        const dataProduct = await fetch("http://localhost:8000/promo", {});
+        const listData = await dataProduct.json();
+        setPromos(listData.result);
+      }
+      useEffect(() => {
+        promo()
+      }, []);
     return(
         <>
             <div className="pt-12 md:mt-48 flex flex-col gap-6">
@@ -21,54 +31,22 @@ function PromoList() {
                     </div>
                 </div>
                 <div className="flex overflow-x-scroll gap-12 md:mx-0 mx-5">
-                    <button className="p-2 bg-[#88B788] flex rounded-2xl flex-shrink-0 ">
-                        <div>
-                            <img src={Coupon} alt="" />
-                        </div>
-                        <div className="flex flex-col gap-5 text-left">
-                            <div>
-                                <div className="font-semibold">HAPPY MOTHER&apos;S DAY!</div>
-                                <div>Get one of our favorite menu for free!</div>
-                            </div>
-                            <div className="text-white">Klaim Kupon</div>
-                        </div>
-                    </button>
-                    <button className="p-2 bg-[#88B788] flex rounded-2xl flex-shrink-0">
-                        <div>
-                            <img src={Coupon} alt="" />
-                        </div>
-                        <div className="flex flex-col gap-5 text-left">
-                            <div>
-                                <div className="font-semibold">HAPPY MOTHER&apos;S DAY!</div>
-                                <div>Get one of our favorite menu for free!</div>
-                            </div>
-                            <div className="text-white">Klaim Kupon</div>
-                        </div>
-                    </button>
-                    <button className="p-2 bg-[#88B788] flex rounded-2xl flex-shrink-0">
-                        <div>
-                            <img src={Coupon} alt="" />
-                        </div>
-                        <div className="flex flex-col gap-5 text-left">
-                            <div>
-                                <div className="font-semibold">HAPPY MOTHER&apos;S DAY!</div>
-                                <div>Get one of our favorite menu for free!</div>
-                            </div>
-                            <div className="text-white">Klaim Kupon</div>
-                        </div>
-                    </button>
-                    <button className="p-2 bg-[#88B788] flex rounded-2xl flex-shrink-0">
-                        <div>
-                            <img src={Coupon} alt="" />
-                        </div>
-                        <div className="flex flex-col gap-5 text-left">
-                            <div>
-                                <div className="font-semibold">HAPPY MOTHER&apos;S DAY!</div>
-                                <div>Get one of our favorite menu for free!</div>
-                            </div>
-                            <div className="text-white">Klaim Kupon</div>
-                        </div>
-                    </button>
+                    {promos.map((item) => {
+                        return (
+                            <button className="p-2 bg-[#88B788] flex rounded-2xl flex-shrink-0 ">
+                                <div>
+                                    <img src={Coupon} alt="" />
+                                </div>
+                                <div className="flex flex-col gap-5 text-left">
+                                    <div>
+                                        <div className="font-semibold">{item.name}</div>
+                                        <div>{item.description}</div>
+                                    </div>
+                                    <div className="text-white">Klaim Kupon</div>
+                                </div>
+                            </button>
+                        );
+                    })}
                 </div>
                 <div className="flex gap-2 md:mx-32 mx-5">
                     <div className="w-7 h-2.5 bg-[#FF8906] rounded-full"></div>
