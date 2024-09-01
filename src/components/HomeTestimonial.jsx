582d275f55b93906364f4b15d2bf6c99e81b1748
fariq <mfariqmaasir@gmail.com>
2024-09-01 10:29:09 +0700

    feat: integration testimonials

--- src/components/HomeTestimonial.jsx
+++ src/components/HomeTestimonial.jsx
@@ -6,36 +6,52 @@ import {
   FaMinus,
   FaCircle,
 } from "react-icons/fa6";
+import { useListTestimonialsQuery } from "../redux/services/testimonial";
 
 function HomeTestimonial() {
+  const [page, setPage] = React.useState(1)
+  if (page < 1) {
+    setPage(1)
+  }
+  const { data, err, isLoading } = useListTestimonialsQuery(page);
+  function StarRating({ rating }) {
+    const stars = Array.from({ length: rating }, (_, index) => (
+      <FaStar key={index} fontSize={20} color="orange" />
+    ));
+  
+    return <div className="flex gap-4">{stars}</div>;
+  }
+  function pageDown() {
+    setPage(page - 1)
+  }
+  function pageUp() {
+    setPage(page + 1)
+  }
   return (
     <>
       <div className="flex flex-col md:flex-row bg-[rgba(0,0,0,0.9)] min-h-[583px] gap-4 py-16 md:px-32 p-5">
-        <div className="bg-white h-[500px] md:max-h-[432px] flex-1"></div>
+        <div className="bg-white h-[500px] md:max-h-[432px] flex-1">
+          <img src={isLoading || err ? "" : data.result.image} alt={isLoading || err ? "" : data.result.name} className="w-full h-full object-cover"/>
+        </div>
         <div className="flex-1 text-white flex-col flex gap-6">
           <div className="text-xl md:flex hidden">TESTIMONIAL</div>
-          <div className="text-3xl">Viezh Robert</div>
-          <div className="text-[#FF8906]">Manager Coffe Shop</div>
+          <div className="text-3xl">{isLoading || err ? "" : data.result.name}</div>
+          <div className="text-[#FF8906]">{isLoading || err ? "" : data.result.profession}</div>
           <div>
-            “Wow... I am very happy to spend my whole day here. the Wi-fi is
-            good, and the coffee and meals tho. I like it here!! Very
-            recommended!
+            {isLoading || err ? "" : data.result.comment}
           </div>
           <div className="flex gap-8 items-center">
-            <span className="flex gap-4 text-[#FF8906]">
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaStar />
-              <FaStar />
-            </span>
-            <span>5.0</span>
+            <div >
+              <StarRating rating={isLoading || err ? "" : data.result.rating} />
+              {/* <FaStar /> */}
+            </div>
+            <span>{isLoading || err ? "" : data.result.rating}</span>
           </div>
           <div className="flex gap-8">
-            <button className="bg-white text-black p-4 rounded-full">
+            <button className="bg-white text-black p-4 rounded-full" onClick={pageDown}>
               <FaArrowLeft />
             </button>
-            <button className="bg-[#FF8906] text-black p-4 rounded-full">
+            <button className="bg-[#FF8906] text-black p-4 rounded-full" onClick={pageUp}>
               <FaArrowRight />
             </button>
           </div>
--- src/redux/services/index.js
+++ src/redux/services/index.js
@@ -1,8 +1,10 @@
 import { products } from "./products";
 import { profile } from "./profile";
+import { testimonials } from "./testimonial";
 
 export const services = {
   [products.reducerPath]: products.reducer,
+  [testimonials.reducerPath]: testimonials.reducer,
 };
 
-export const rtkMiddlewares = [products.middleware];
+export const rtkMiddlewares = [products.middleware, testimonials.middleware];
