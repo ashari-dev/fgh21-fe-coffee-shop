c61070485c306504892042cf0cb7475d3a47b815
fariq <mfariqmaasir@gmail.com>
2024-09-02 01:06:14 +0700

    feat: repair services and store

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import auth from "./auth";
 import profile from "./profile";
+import { services } from "../services";
 
 const reducer = combineReducers({
   auth,
   profile,
+  ...services,
 });
 
 export default reducer;
